7d886c14f60fdab2af168d8ff00b6510a361b0f8
Bartosz Kościów <kosci1@gmail.com>
2017-03-11 12:59:49 +0100

    relay handler

--- parameters.dist.lua
+++ parameters.dist.lua
@@ -2,7 +2,7 @@ NET_AP = "---"
 NET_PASSWORD = "---"
 PROTOCOL = "iot:1"
 PORT = 5053
-
+CHANNELS = {2, 3, 4, 1}
 if file.exists('parameters-device.lc') then  
     dofile("parameters-device.lc")        
 else

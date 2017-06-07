2416c4545c4b8a3027628c5554e996737ee5077c
Code1110 <sk@skhome.ch>
2017-06-08 00:07:01 +0200

    * fixed i2c (drv:_write4 was not sending correct bits) * added Backlight support for i2c * setCustomChar now also works with i2c

--- lcd_hd44780_i2c.lua
+++ lcd_hd44780_i2c.lua
@@ -16,6 +16,7 @@ function drv.new(addr, sda, scl, pins)
     self.scl = scl
     self._rs = 0
     self._buffer = nil
+    self._blon = true
     if pins == nil then
         self.pins = {
             RS= 4,
@@ -25,6 +26,7 @@ function drv.new(addr, sda, scl, pins)
             DB5= 1,
             DB6= 2,
             DB7= 3,
+            BL= nil
         }
     else
         self.pins = pins
@@ -54,7 +56,6 @@ function drv:_write4(ch, enable)
     if bit.isset(ch, 1) then self._buffer = self._buffer + math.pow(2, self.pins['DB5']) end        
     if bit.isset(ch, 2) then self._buffer = self._buffer + math.pow(2, self.pins['DB6']) end       
     if bit.isset(ch, 3) then self._buffer = self._buffer + math.pow(2, self.pins['DB7']) end
-    self._buffer = ch
     drv._send(self, enable)
 end
 
@@ -63,19 +64,39 @@ function drv:_write8(ch, enable)
     drv._write4(self, bit.band(ch, 0x0F), enable)
 end
 
+function drv:_getblpin()
+    if (self.pins['BL'] == nil or self._blon == false) then
+        return 0
+    else
+        return math.pow(2, self.pins['BL'])
+    end
+end
+
 function drv:_send(enable)  
     enable = "E"..enable
     i2c.start(self.port)
-    i2c.address(self.port, self.addr, i2c.TRANSMITTER)    
-    i2c.write(self.port, self._buffer +  math.pow(2, self.pins[enable]) + self._rs)
+    i2c.address(self.port, self.addr, i2c.TRANSMITTER)
+    i2c.write(self.port, self._buffer +  math.pow(2, self.pins[enable]) + self._rs + drv._getblpin(self))
     tmr.delay(5)
-    i2c.write(self.port, self._buffer)
+    i2c.write(self.port, self._buffer + drv._getblpin(self))
     i2c.stop(self.port)
 end
 
 function drv:write(ch, enable)
     self._rs =  math.pow(2, self.pins['RS'])
     self._write8(self, ch:byte(1), enable)        
+end
+
+function drv:backlight(on)
+    self._blon = on
+    i2c.start(self.port)
+    i2c.address(self.port, self.addr, i2c.TRANSMITTER)
+    i2c.write(self.port, drv._getblpin(self))
+    i2c.stop(self.port)
+end
+
+function drv:setrs(high)
+    if (high) then self._rs = math.pow(2, self.pins['RS']) else self._rs = 0 end
 end
 
 return drv

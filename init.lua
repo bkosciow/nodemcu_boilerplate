b9f60ad915c80e228e9eb48637cbd8a2a2e82839
Bartosz Kościów <kosci1@gmail.com>
2017-08-09 20:27:15 +0200

    rc522 upgrade

--- init.lua
+++ init.lua
@@ -1,7 +1,14 @@
 print ("Booting..")
 gpio.mode(0, gpio.INPUT, gpio.PULLUP)
+gpio.mode(1, gpio.INPUT, gpio.PULLUP)
 if gpio.read(0) == 0 then
     print("..aborted")
+elseif gpio.read(1) == 0 then
+    if file.exists('configurator.lc') then  
+        dofile("configurator.lc")        
+    else
+        dofile("configurator.lua")        
+    end  
 else    
     if file.exists('parameters.lc') then  
         dofile("parameters.lc")        
